b1c87d586b337876074ad78f5fd8353c07d02ecb
rajaswamy.s <rajaswamy2004@gmail.com>
2025-06-09 11:34:17 +0530

    added new file

--- main.py
+++ main.py
@@ -12,19 +12,20 @@
 #         if row[1] != "temp":
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
-
-import pandas
-
-data = pandas.read_csv("weather_data .csv")
-# print((type(data)))
 #
-# data_dist = data.to_dict()
-# print(data_dist)
-# temp_list = data["temp"].to_list()
-# print(len(temp_list))
+# import pandas
 #
-# print(data["temp"].max())
-# print(data["temp"])
-
-max_temp = data["temp"].max()
-print(data[data.temp == data.temp.max()])
+# data = pandas.read_csv("weather_data .csv")
+# # print((type(data)))
+# #
+# # data_dist = data.to_dict()
+# # print(data_dist)
+# # temp_list = data["temp"].to_list()
+# # print(len(temp_list))
+# #
+# # print(data["temp"].max())
+# # print(data["temp"])
+#
+# max_temp = data["temp"].max()
+# print(data[data.temp == data.temp.max()])
+# print(data[data.day == "Monday"])
